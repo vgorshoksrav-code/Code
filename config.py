810efee78ba6a8a6ebb90d179b97f647fc6e7886
vgorshoksrav-code <vgorshoksrav@gmail.com>
2026-05-01 16:50:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ BOT_TOKEN = "8762188358:AAGIcDmptaMV3oLcHfs78EV5CMTW-OcPSMI"
 SUPER_ADMINS = [8757558620]  # ваш Telegram ID
 
 # Путь к базе данных
-DB_PATH = "data/echaris.db"
+DB_PATH = "/data/echaris.db"
